b18ac21856c0ee2668393ebdaeeb2c9338c2becd
Kwai Hung <kwaihung1314@hotmail.com>
2018-09-19 21:28:11 +0800

    divide into gp & private components; added private chat feature; modify online array so wont renew everytime updated

--- src/components/chatroom.vue
+++ src/components/chatroom.vue
@@ -1,74 +1,68 @@
 <template>
 <v-app>
-  <v-container grid-list-md>
+  <v-container>
     <h1 class="app-header">Edvant Chatroom</h1>
     <h2 class="user-header mb-4">Your name: <span class="cyan--text">{{username}}</span></h2>
     <v-layout wrap>
       <v-flex xs12>
         <div class="teal lighten-3 online-box pa-3">
           <div class="header">Currently online</div>
-          <v-chip v-for="(user, index) in onlineList" :key="index">
-            <v-avatar class="teal">{{user.substr(0,1)}}</v-avatar>
-            {{user}}
+          <v-chip v-for="(user, index) in onlineArr" :key="index">
+            <v-avatar class="teal">{{user.name.substr(0,1).toUpperCase()}}</v-avatar>
+            {{user.name}}
           </v-chip>
           <div class="typing" v-show="typingPerson">{{typingPerson}} is typing...</div>
         </div>
       </v-flex>
+    </v-layout>
+    <v-layout wrap>
       <v-flex xs12>
-        <div class="chat-box mt-4">
-          <div class="message-box pa-4 blue lighten-3">
-            <div class="message-window">
-              <div v-for="(message, index) in messageList"
-                :key="index">
-                <div class="message-block mb-2"
-                  v-if="message.type === 'newMsg'"
-                  :class="{'others-message': socket.id !== message.userId}">
-                  <div class="message">
-                    <div class="senderName">{{message.username}}</div><div class="text" v-html="message.content.replace(/\n/g, '<br/>')"></div>
-                    <img :id="message.image" v-if="message.image" :src="message.image">
-                    <div class="time">{{message.time}}</div>
-                  </div>
-                </div>
-                <div class="enterLeftMsg-block mb-2"
-                  v-if="message.type === 'enter' || message.type === 'left'">
-                  <div class="message"
-                    :class="{enter: message.type === 'enter'}">{{message.content}}</div>
-                </div>
-              </div>
-            </div>
+        <group-chatbox
+          ref="group"
+          :socket="socket"
+          :username="username"/>
+      </v-flex>
+    </v-layout>
+    <v-layout wrap class="mt-4">
+      <v-flex xs3>
+        <div class="onlineSide teal lighten-3">
+          <div class="header teal lighten-4 pa-2">
+            <span>Private Chat</span>
           </div>
-          <div class="typing-box blue lighten-4">
-            <form action="" @submit.prevent="sendMessage">
-              <v-layout wrap>
-                <v-flex xs8 sm9 md10 class="input-box pa-3">
-                  <textarea placeholder="Enter message" v-model="messageInput" @keydown="textareaKeyDown" @keyup="textareaKeyUp"
-                    @input="sendTyping"></textarea>
-                </v-flex>
-                <v-flex xs4 sm3 md2 align-center justify-center class="send-box">
-                  <input type="file" id="imageInput" accept="image/*" v-show="false" @input="viewSendImage($event)">
-                  <v-btn fab light small color="#E53935" @click="triggerImage">
-                    <v-icon color="#ffffff" size="20">photo_library</v-icon>
-                  </v-btn>
-                  <v-btn color="#FFB300" type="submit">Send</v-btn>
-                </v-flex>
-              </v-layout>
-            </form>
-          </div>
+          <v-list class="teal lighten-3">
+            <v-list-tile
+              v-for="(user, index) in onlineArr"
+              :key="index"
+              avatar
+              @click="selectChat(index)">
+              <v-list-tile-action>
+                <v-icon color="#FFB300" v-show="user.flag">priority_high</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title v-text="user.name"></v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-avatar>
+                <v-avatar class="teal" size="40">{{user.name.substr(0,1).toUpperCase()}}</v-avatar>
+              </v-list-tile-avatar>
+            </v-list-tile>
+          </v-list>
+        </div>
+      </v-flex>
+      <v-flex xs9>
+        <private-chatbox
+          v-for="(user, index) in onlineArr"
+          :key="index"
+          :ref="user.id"
+          v-show="selectedChat === index"
+          :socket="socket"
+          :username="username"
+          :boxId="user.id"
+          :boxPerson="user.name"/>
+        <div class="emptyChat blue lighten-4 px-3" v-show="selectedChat === null">
+          <div class="statement">{{emptyChatName}} left the chatroom. Please select from the list to process other chat.</div>
         </div>
       </v-flex>
     </v-layout>
-    <v-dialog
-      v-model="openImageModel"
-      max-width="500"
-      @input="v => v || clearImageModel()">
-      <div class="content">
-        <img :src="imageSelected.url">
-        <v-divider></v-divider>
-        <div class="action">
-          <v-btn color="#FFB300" @click="sendImage">Send</v-btn>
-        </div>
-      </div>
-    </v-dialog>
   </v-container>
 </v-app>
 </template>
@@ -76,106 +70,46 @@
 <script>
 import io from 'socket.io-client'
 import debounce from 'lodash.debounce'
+import groupChatbox from '@/components/groupChatbox'
+import privateChatbox from '@/components/privateChatbox'
 export default {
   data () {
     return {
-      onlineList: [],
-      messageList: [],
-      socket: io('http://10.0.1.7:3000'),
+      onlineArr: [],
+      socket: io('http://127.0.0.1:3000'),
       username: '',
-      messageInput: '',
       noticeCount: 0,
       inFocus: true,
       docTitle: 'chat_vue',
-      textKey: {
-        16: false,
-        13: false
-      },
       typingPerson: null,
-      EnterLeftMsg: '',
-      imageSelected: {
-        file: '',
-        url: ''
-      },
-      openImageModel: false
+      // private chat index, default [0]
+      selectedChat: 0,
+      emptyChatName: ''
     }
   },
+  components: {
+    groupChatbox,
+    privateChatbox
+  },
   methods: {
-    sendMessage () {
-      let msgTime = new Date()
-      this.socket.emit('message', {
-        userId: this.socket.id,
-        username: this.username,
-        content: this.messageInput,
-        time: `${msgTime.getHours().toString().length > 1 ? msgTime.getHours() : ('0' + msgTime.getHours())}:${msgTime.getMinutes().toString().length > 1 ? msgTime.getMinutes() : ('0' + msgTime.getMinutes())}`
-      })
-      this.messageInput = ''
-    },
-    sendTyping () {
-      this.socket.emit('typing', this.username)
-    },
     updateTitle () {
       if (this.inFocus) {
         document.title = this.docTitle
+        this.noticeCount = 0
       } else {
-        document.title = `(${this.noticeCount})${this.docTitle}`
+        document.title = `(${this.noticeCount}) ${this.docTitle}`
       }
     },
     delayRemoveTyping: debounce(function () {
       this.typingPerson = null
     }, 500),
-    delayRemoveOnline: debounce(function () {
-      this.EnterLeftMsg = ''
-    }, 1000),
-    textareaKeyDown (e) {
-      e = e || event
-      if (e.keyCode in this.textKey) {
-        this.textKey[e.keyCode] = true
-        if (this.textKey['16'] && this.textKey['13']) {
-          return
-        }
-        if (this.textKey['13']) {
-          e.preventDefault()
-          if (this.messageInput) {
-            this.sendMessage()
-          }
-        }
-      }
+    selectChat (index) {
+      this.selectedChat = index
+      this.onlineArr[index].flag = false
     },
-    textareaKeyUp (e) {
-      e = e || event
-      if (e.keyCode in this.textKey) {
-        this.textKey[e.keyCode] = false
-      }
-    },
-    autoscroll () {
-      let messageBox = document.querySelector('.message-box')
-      messageBox.scrollTop = messageBox.scrollHeight - messageBox.clientHeight
-    },
-    viewSendImage (e) {
-      e = e || event
-      this.imageSelected.file = e.target.files[0]
-      this.imageSelected.url = window.URL.createObjectURL(e.target.files[0])
-      this.openImageModel = true
-      document.getElementById('imageInput').value = ''
-    },
-    triggerImage () {
-      document.getElementById('imageInput').click()
-    },
-    clearImageModel () {
-      this.openImageModel = false
-      window.URL.revokeObjectURL(this.imageSelected.url)
-      this.imageSelected.url = ''
-      this.imageSelected.file = ''
-    },
-    sendImage () {
-      let msgTime = new Date()
-      this.socket.emit('image', {
-        userId: this.socket.id,
-        username: this.username,
-        time: `${msgTime.getHours().toString().length > 1 ? msgTime.getHours() : ('0' + msgTime.getHours())}:${msgTime.getMinutes().toString().length > 1 ? msgTime.getMinutes() : ('0' + msgTime.getMinutes())}`
-      }, this.imageSelected.file)
-      this.clearImageModel()
+    showEmptyChat (name) {
+      this.emptyChatName = name
+      this.selectedChat = null
     }
   },
   mounted () {
@@ -183,31 +117,47 @@ export default {
     this.socket.on('connect', () => {
       this.socket.emit('user entered', this.username)
     })
-    this.socket.on('update users', (userList, msg, type) => {
-      this.onlineList = []
-      for (let key in userList) {
-        this.onlineList.push(userList[key])
+    this.socket.on('online user', (list) => {
+      for (let key in list) {
+        this.onlineArr.push({
+          name: list[key],
+          id: key,
+          flag: false
+        })
       }
-      this.messageList.push({
+    })
+    this.socket.on('update users', (user, msg, type) => {
+      if (type === 'enter' && user.id !== this.socket.id) {
+        this.onlineArr.push({
+          name: user.name,
+          id: user.id,
+          flag: false
+        })
+      } else {
+        for (let i = 0; i < this.onlineArr.length; i++) {
+          if (this.onlineArr[i].id === user.id) {
+            if (this.selectedChat === i) {
+              this.showEmptyChat(this.onlineArr[i].name)
+            }
+            this.onlineArr.splice(i, 1)
+            break
+          }
+        }
+      }
+      this.$refs['group'].pushMessage({
         content: msg,
         userId: '',
         username: '',
         type: type
       })
-      this.$nextTick(() => {
-        this.autoscroll()
-      })
     })
-    this.socket.on('new message', (message) => {
-      this.messageList.push({
+    this.socket.on('new group message', (message) => {
+      this.$refs['group'].pushMessage({
         content: message.content,
         userId: message.userId,
         username: message.username,
         type: 'newMsg',
         time: message.time
-      })
-      this.$nextTick(() => {
-        this.autoscroll()
       })
       this.noticeCount++
       this.updateTitle()
@@ -216,7 +166,7 @@ export default {
       this.typingPerson = user
       this.delayRemoveTyping()
     })
-    this.socket.on('new image', (message, buffer) => {
+    this.socket.on('new group image', (message, buffer) => {
       let file = new Blob([buffer])
       let fileObj = {
         content: '',
@@ -226,9 +176,36 @@ export default {
         type: 'newMsg',
         time: message.time
       }
-      this.messageList.push(fileObj)
-      this.$nextTick(() => {
-        document.getElementById(fileObj.image).onload = this.autoscroll
+      this.$refs['group'].pushImage(fileObj)
+      this.noticeCount++
+      this.updateTitle()
+    })
+    // event of ppl sent you private msg
+    this.socket.on('new private message', (message) => {
+      this.$refs[(message.fromId)][0].pushMessage({
+        content: message.content,
+        userId: message.fromId,
+        type: 'newMsg',
+        time: message.time
+      })
+      for (let i = 0; i < this.onlineArr.length; i++) {
+        if (this.onlineArr[i].id === message.fromId) {
+          if (this.selectedChat !== i) {
+            this.onlineArr[i].flag = true
+          }
+          break
+        }
+      }
+      this.noticeCount++
+      this.updateTitle()
+    })
+    // event of you private msg other ppl
+    this.socket.on('my private message', (message) => {
+      this.$refs[(message.toId)][0].pushMessage({
+        content: message.content,
+        userId: message.fromId,
+        type: 'newMsg',
+        time: message.time
       })
     })
     window.onfocus = () => {
@@ -239,6 +216,11 @@ export default {
     window.onblur = () => {
       this.inFocus = false
     }
+    let self = this
+    setInterval(() => {
+      self.socket.emit('ping')
+      console.log('emit ping')
+    }, 10000)
   },
   beforeDestroy () {
     this.socket.close()
@@ -265,103 +247,27 @@ export default {
   }
 }
 
-.chat-box {
-  border-radius: 8px;
-  .message-box {
-    border-radius: 8px 8px 0px 0px;
-    height: 450px;
-    overflow-y: auto;
-    .message-window {
-      height: 100%;
-    }
-    &::-webkit-scrollbar {
-      width: 5px;
-      border-radius: 5px;
-    }
-    &::-webkit-scrollbar-track {
-      background: #e1e1e1;
-    }
-    &::-webkit-scrollbar-thumb {
-      background: #afafaf;
-      border-radius: 1.5px;
-    }
-  }
-  .typing-box {
-    border-radius: 0px 0px 8px 8px;
-    .input-box {
-      textarea{
-        width: 100%;
-        height: 40px;
-        outline: none;
-        background-color: #fff;
-        border-radius: 10px;
-        padding: 8px;
-      }
-    }
-    .send-box {
-      width: 60px;
-      display: flex;
+.onlineSide {
+  height: 100%;
+  border-radius: 8px 0px 0px 8px;
+  overflow-y: auto;
+  .header {
+    border-radius: 8px 0px 0px 0px;
+    span {
+      font-size: 15px;
     }
   }
 }
 
-.message-window {
-  .message-block {
-    display: flex;
-    justify-content: flex-end;
-    .message {
-      border-radius: 5px;
-      background-color: #fff;
-      padding: 5px;
-      max-width: 250px;
-      word-break: break-all;
-      white-space: pre-wrap;
-      img {
-        width: 100%;
-      }
-      .time {
-        float: right;
-        font-size: 10px;
-        color: #9f9f9f;
-        margin-top: 5px;
-      }
-    }
-  }
-  .enterLeftMsg-block {
-    display: flex;
-    justify-content: center;
-    .message {
-      border-radius: 5px;
-      background-color: rgba(0, 0, 0, 0.1);
-      color: #888;
-      padding: 5px;
-      &.enter {
-        background-color: rgba(247, 201, 64, 0.3)
-      }
-    }
-  }
-  .others-message {
-    justify-content: flex-start;
-  }
-  .senderName {
-    color: #9f9f9f;
-    font-weight: 500;
-    font-size: 15px;
-  }
-}
-
-.v-dialog {
-  .content {
-    background-color: #fafafa;
-    padding: 10px;
-    img {
-      width: 480px;
-      margin-bottom: 10px;
-    }
-    .action {
-      margin-top: 10px;
-      text-align: right;
-    }
+.emptyChat {
+  height: 566px;
+  border-radius: 0px 8px 8px 0px;
+  visibility: 0.5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .statement {
+    text-align: center;
   }
 }
 </style>
---
+++ src/components/privateChatbox.vue
@@ -0,0 +1,261 @@
+<template>
+  <div class="chat-box">
+    <div class="box-person blue lighten-4 pa-2">
+      <span class="ml-3">{{boxPerson}}</span>
+    </div>
+    <div class="message-box pa-4 blue lighten-3">
+      <div class="message-window">
+        <div v-for="(message, index) in messageList"
+          :key="index">
+          <div class="message-block mb-2"
+            v-if="message.type === 'newMsg'"
+            :class="{'others-message': socket.id !== message.userId}">
+            <div class="message">
+              <div class="text" v-html="message.content.replace(/\n/g, '<br/>')"></div>
+              <img :id="message.image" v-if="message.image" :src="message.image">
+              <div class="time">{{message.time}}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="typing-box blue lighten-4">
+      <form action="" @submit.prevent="sendMessage">
+        <v-layout wrap>
+          <v-flex xs7 sm9 class="input-box pa-3">
+            <textarea placeholder="Enter message" v-model="messageInput" @keydown="textareaKeyDown" @keyup="textareaKeyUp"></textarea>
+          </v-flex>
+          <v-flex xs5 sm3 align-center justify-center class="send-box">
+            <input type="file" id="imageInput" accept="image/*" v-show="false" @input="viewSendImage($event)">
+            <v-btn fab light small color="#E53935" @click="triggerImage">
+              <v-icon color="#ffffff" size="20">photo_library</v-icon>
+            </v-btn>
+            <v-btn color="#FFB300" type="submit">Send</v-btn>
+          </v-flex>
+        </v-layout>
+      </form>
+    </div>
+    <v-dialog
+      v-model="openImageModel"
+      max-width="500"
+      @input="v => v || clearImageModel()">
+      <div class="content">
+        <img :src="imageSelected.url">
+        <v-divider></v-divider>
+        <div class="action">
+          <v-btn color="#FFB300" @click="sendImage">Send</v-btn>
+        </div>
+      </div>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      messageList: [],
+      messageInput: '',
+      textKey: {
+        16: false,
+        13: false
+      },
+      imageSelected: {
+        file: '',
+        url: ''
+      },
+      openImageModel: false
+    }
+  },
+  props: {
+    socket: Object,
+    username: String,
+    boxId: String, // socket ID of the person you are talking to
+    boxPerson: String // name of the person you are talking to
+  },
+  methods: {
+    sendMessage () {
+      let msgTime = new Date()
+      this.socket.emit('private message', {
+        fromId: this.socket.id,
+        toId: this.boxId,
+        content: this.messageInput,
+        time: `${msgTime.getHours().toString().length > 1 ? msgTime.getHours() : ('0' + msgTime.getHours())}:${msgTime.getMinutes().toString().length > 1 ? msgTime.getMinutes() : ('0' + msgTime.getMinutes())}`
+      })
+      this.messageInput = ''
+    },
+    pushMessage (msgObj) {
+      this.messageList.push(msgObj)
+      this.$nextTick(() => {
+        this.autoscroll()
+      })
+    },
+    sendImage () {
+      let msgTime = new Date()
+      this.socket.emit('private image', {
+        fromId: this.socket.id,
+        toId: this.boxId,
+        time: `${msgTime.getHours().toString().length > 1 ? msgTime.getHours() : ('0' + msgTime.getHours())}:${msgTime.getMinutes().toString().length > 1 ? msgTime.getMinutes() : ('0' + msgTime.getMinutes())}`
+      }, this.imageSelected.file)
+      this.clearImageModel()
+    },
+    pushImage (fileObj) {
+      this.messageList.push(fileObj)
+      this.$nextTick(() => {
+        document.getElementById(fileObj.image).onload = this.autoscroll
+      })
+    },
+    // sendTyping () {
+    //   this.socket.emit('typing', this.username)
+    // },
+    textareaKeyDown (e) {
+      e = e || event
+      if (e.keyCode in this.textKey) {
+        this.textKey[e.keyCode] = true
+        if (this.textKey['16'] && this.textKey['13']) {
+          return
+        }
+        if (this.textKey['13']) {
+          e.preventDefault()
+          if (this.messageInput) {
+            this.sendMessage()
+          }
+        }
+      }
+    },
+    textareaKeyUp (e) {
+      e = e || event
+      if (e.keyCode in this.textKey) {
+        this.textKey[e.keyCode] = false
+      }
+    },
+    autoscroll () {
+      let messageBox = this.$el.querySelector('.message-box')
+      messageBox.scrollTop = messageBox.scrollHeight - messageBox.clientHeight
+    },
+    viewSendImage (e) {
+      e = e || event
+      this.imageSelected.file = e.target.files[0]
+      this.imageSelected.url = window.URL.createObjectURL(e.target.files[0])
+      this.openImageModel = true
+      this.$el.querySelector('#imageInput').value = ''
+    },
+    triggerImage () {
+      this.$el.querySelector('#imageInput').click()
+    },
+    clearImageModel () {
+      this.openImageModel = false
+      window.URL.revokeObjectURL(this.imageSelected.url)
+      this.imageSelected.url = ''
+      this.imageSelected.file = ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.chat-box {
+  border-radius: 8px;
+  .box-person {
+    border-radius: 0px 8px 0px 0px;
+    span {
+      font-size: 15px;
+    }
+  }
+  .message-box {
+    height: 450px;
+    overflow-y: auto;
+    .message-window {
+      height: 100%;
+    }
+    &::-webkit-scrollbar {
+      width: 5px;
+      border-radius: 5px;
+    }
+    &::-webkit-scrollbar-track {
+      background: #e1e1e1;
+    }
+    &::-webkit-scrollbar-thumb {
+      background: #afafaf;
+      border-radius: 1.5px;
+    }
+  }
+  .typing-box {
+    border-radius: 0px 0px 8px 0px;
+    .input-box {
+      textarea{
+        width: 100%;
+        height: 40px;
+        outline: none;
+        background-color: #fff;
+        border-radius: 10px;
+        padding: 8px;
+      }
+    }
+    .send-box {
+      width: 60px;
+      display: flex;
+    }
+  }
+}
+
+.message-window {
+  .message-block {
+    display: flex;
+    justify-content: flex-end;
+    .message {
+      border-radius: 5px;
+      background-color: #fff;
+      padding: 5px;
+      max-width: 250px;
+      word-break: break-all;
+      white-space: pre-wrap;
+      img {
+        width: 100%;
+      }
+      .time {
+        float: right;
+        font-size: 10px;
+        color: #9f9f9f;
+        margin-top: 5px;
+      }
+    }
+  }
+  .enterLeftMsg-block {
+    display: flex;
+    justify-content: center;
+    .message {
+      border-radius: 5px;
+      background-color: rgba(0, 0, 0, 0.1);
+      color: #888;
+      padding: 5px;
+      &.enter {
+        background-color: rgba(247, 201, 64, 0.3)
+      }
+    }
+  }
+  .others-message {
+    justify-content: flex-start;
+  }
+  .senderName {
+    color: #9f9f9f;
+    font-weight: 500;
+    font-size: 15px;
+  }
+}
+
+.v-dialog {
+  .content {
+    background-color: #fafafa;
+    padding: 10px;
+    img {
+      width: 480px;
+      margin-bottom: 10px;
+    }
+    .action {
+      margin-top: 10px;
+      text-align: right;
+    }
+  }
+}
+</style>
